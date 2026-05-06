f585a948d1e4f567f2b20fd8c96101f4e25ac30c
slaveyametodieva <slaveyamil@abv.bg>
2026-05-07 00:44:21 +0300

    tickets tasks

---
+++ 3rd_homework/3rd_homework/testTickets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "StudentTicket.h"
+#include "GroupTicket.h"
+
+
+int main()
+{
+	try
+	{
+		//StudentTicket t("Pinocio", -50);
+		GroupTicket six("Ariel", 20);
+		std::cout << six << '\n';
+	}
+
+	catch (const std::exception& e)
+	{
+		std::cerr << "Problem occured: " << e.what() << '\n';
+	}
+	return 0;
+}
